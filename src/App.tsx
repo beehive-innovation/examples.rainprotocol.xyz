71973af95d20c7fd961ce7becbb39c4e5e8df129
Timothy Coleman <timhc22@gmail.com>
2022-02-02 16:49:03 +0000

    Add trust factories call

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 // import Web3 from 'web3';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 function App() {
+
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     getData();
@@ -12,7 +21,7 @@ function App() {
   async function getData() {
 
     // desc seems to show the most recent
-    let data = await fetch(`https://api.thegraph.com/subgraphs/name/vishalkale151071/rain-protocol`, {
+    let res = await fetch(`https://api.thegraph.com/subgraphs/name/vishalkale151071/rain-protocol`, {
       method: 'POST',
       headers: {
         // 'Accept': 'application/json',
@@ -42,13 +51,41 @@ function App() {
     //   }
     // }
 
-    data = await data.json();
-    console.log(data);
+    res = await res.json();
+    console.log(res);
+    // @ts-ignore
+    setData(res.data.trustFactories);
   }
 
 
   return (
     <div>
+      <h2>Trust Factories</h2>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell align="right">Trust Count</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((row: any) => (
+              <TableRow
+                key={row.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell align="right">{row.trustCount}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
     </div>
   );
 }
