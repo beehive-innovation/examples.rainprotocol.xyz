b72093a64924699e60d46860ec5c133851e7841c
Timothy Coleman <timhc22@gmail.com>
2022-03-26 02:28:44 +0000

    Add placeholder for OpcodeCalculatorExample

---
+++ src/examples/OpcodeCalculatorExample/OpcodeCalculatorExample.tsx
@@ -0,0 +1,90 @@
+import '../../App.css';
+import React, {useState} from 'react';
+import { networks } from "./networks";
+// import { isAfterBlockNumber } from "./opcodeExample";
+import {ContractInterface, ethers} from 'ethers';
+import calculatorTest from "./calculatorTestABI.json";
+import {Button, Typography} from "@mui/material";
+
+export default function OpcodeCalculatorExample({}: any) {
+
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [result, setResult] = useState("Result: ");
+
+  const connectWalletHandler = async () => {
+    // @ts-ignore
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("No Web3 Wallet installed");
+    }
+
+    try {
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+        chainId: networks[0].config.chainId,
+      });
+      console.log(`Address ${accounts[0]} connected`);
+      setCurrentAccount(accounts[0]);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const runOpcodesExample = async () => {
+    // @ts-ignore
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("No Web3 Wallet installed");
+    }
+
+    try {
+      // const contractInstance = new ethers.Contract(saleContractABI.abi as AbiItem[], networks[0].addresses.SALE_EXAMPLE, ethereum);
+
+
+      // todo will use typechain
+      const provider = new ethers.providers.Web3Provider(ethereum, "any");
+      // Prompt user for account connections
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      console.log("Account:", await signer.getAddress());
+
+      // @ts-ignore
+      const contractInstance = new ethers.Contract(networks[0].addresses.SALE_EXAMPLE, saleContractABI.abi, signer);
+      const price = await contractInstance.calculatePrice(2);
+
+      console.log(price);
+      setResult(`Result: ${price._hex}`);
+
+      // TODO ADD FUNCTIONALITY FOR CALLING THE SMART CONTRACT WITH THE OPCODE
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant="contained" onClick={connectWalletHandler}>
+          Connect
+        </Button>
+
+        <br/>
+
+        <Typography>{`Connected as: ${currentAccount}`}</Typography>
+
+        <br/>
+
+        <Button variant="contained" onClick={runOpcodesExample}>
+          Run Sale Example
+        </Button>
+
+        <br/>
+
+        <Typography>{result}</Typography>
+      </header>
+    </div>
+  )
+}
