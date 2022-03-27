374839593ead25c3ffa21f370697cf3ce6061486
Timothy Coleman <timhc22@gmail.com>
2022-03-27 02:57:11 +0100

    Add other needed data for deployment

--- src/examples/DeploySaleExample/DeploySaleExample.tsx
+++ src/examples/DeploySaleExample/DeploySaleExample.tsx
@@ -1,12 +1,89 @@
 import '../../App.css';
 import React, {useState} from 'react';
 import { networks } from "./networks";
-import {ethers} from 'ethers';
+import {Contract, ethers} from 'ethers';
 import saleFactoryABI from "./saleFactoryABI.json";
+import saleContractABI from "./saleContractABI.json";
 import defaults from "./defaults.json";
 import {Button, Divider, Link, Typography} from "@mui/material";
 import SaleForm from "./SaleForm";
 import RedeemableForm from "./RedeemableForm";
+import {concat} from "ethers/lib/utils";
+import { op } from './utils';
+
+
+export const enum Opcode {
+  SKIP,
+  VAL,
+  DUP,
+  ZIPMAP,
+  BLOCK_NUMBER,
+  BLOCK_TIMESTAMP,
+  SENDER,
+  IS_ZERO,
+  EAGER_IF,
+  EQUAL_TO,
+  LESS_THAN,
+  GREATER_THAN,
+  EVERY,
+  ANY,
+  ADD,
+  SUB,
+  MUL,
+  DIV,
+  MOD,
+  POW,
+  MIN,
+  MAX,
+  REPORT,
+  NEVER,
+  ALWAYS,
+  SATURATING_DIFF,
+  UPDATE_BLOCKS_FOR_TIER_RANGE,
+  SELECT_LTE,
+  ERC20_BALANCE_OF,
+  ERC20_TOTAL_SUPPLY,
+  ERC721_BALANCE_OF,
+  ERC721_OWNER_OF,
+  ERC1155_BALANCE_OF,
+  ERC1155_BALANCE_OF_BATCH,
+  REMAINING_UNITS,
+  TOTAL_RESERVE_IN,
+  LAST_BUY_BLOCK,
+  LAST_BUY_UNITS,
+  LAST_BUY_PRICE,
+  CURRENT_BUY_UNITS,
+  TOKEN_ADDRESS,
+  RESERVE_ADDRESS,
+}
+
+export const getNewChildFromReceipt = (receipt: any, parentContract: any) => {
+  return ethers.utils.defaultAbiCoder.decode(
+    ["address", "address"],
+    receipt.events.filter(
+      (event: any) =>
+        event.event == "NewChild" &&
+        event.address.toUpperCase() == parentContract.address.toUpperCase()
+    )[0].data
+  )[1];
+};
+
+export const afterTimestampConfig = (timestamp: any) => {
+  return {
+    sources: [
+      concat([
+        // (BLOCK_NUMBER blockNumberSub1 gt)
+        op(Opcode.BLOCK_TIMESTAMP),
+        op(Opcode.VAL, 0),
+        op(Opcode.GREATER_THAN),
+      ]),
+    ],
+    constants: [timestamp],
+    stackLength: 3,
+    argumentsLength: 0,
+  };
+};
+
 
 /**
  * DeploySaleExample
@@ -64,8 +141,6 @@ export default function DeploySaleExample({}: any) {
     }
 
     try {
-      console.log('Submitting the following state:', saleState, redeemableState);
-
       const provider = new ethers.providers.Web3Provider(ethereum, {
         name: networks[0].config.chainName,
         chainId: parseInt(networks[0].config.chainId),
@@ -78,11 +153,99 @@ export default function DeploySaleExample({}: any) {
 
       // todo will use typechain
       // @ts-ignore
-      const contractInstance = new ethers.Contract(networks[0].addresses.SALE_FACTORY, saleFactoryABI.abi, signer);
-      const deployedAddress = await contractInstance.createChildTyped(saleState, redeemableState);
+      const saleFactory = new ethers.Contract(
+        networks[0].addresses.SALE_FACTORY,
+        saleFactoryABI.abi,
+        signer
+      );
 
-      console.log(deployedAddress);
-      // setResult(`Result: ${price._hex}`);
+
+      const staticPrice = 100; // todo this might not work and is not currently retreived dynamically here from a reserveErc20
+      const walletCap = 10; // too see above
+
+      const constants = [staticPrice, walletCap, ethers.constants.MaxUint256];
+      const sources = [
+        concat([
+          op(Opcode.CURRENT_BUY_UNITS),
+          op(Opcode.TOKEN_ADDRESS),
+          op(Opcode.SENDER),
+          op(Opcode.ERC20_BALANCE_OF),
+          op(Opcode.ADD, 2),
+          op(Opcode.VAL, 1),
+          op(Opcode.GREATER_THAN),
+          op(Opcode.VAL, 2),
+          op(Opcode.VAL, 0),
+          op(Opcode.EAGER_IF),
+        ]),
+      ];
+
+      let raiseRange;
+
+      let extendedSaleState: any = saleState;
+      extendedSaleState.canStartStateConfig = afterTimestampConfig(
+        // @ts-ignore
+        Math.floor(raiseRange?.[0].$d.getTime() / 1000)
+      )
+      extendedSaleState.canEndStateConfig = afterTimestampConfig(
+        // @ts-ignore
+        Math.floor(raiseRange?.[1].$d.getTime() / 1000)
+      )
+      extendedSaleState.calculatePriceStateConfig = {
+        sources,
+        constants,
+        stackLength: 10,
+        argumentsLength: 0
+      }
+      extendedSaleState.dustSize = 0;
+
+      // big numbers
+      // extendedSaleState.cooldownDuration = ethers.utils.parseUnits(extendedSaleState.cooldownDuration.toString())
+      extendedSaleState.minimumRaise = ethers.utils.parseUnits(extendedSaleState.minimumRaise.toString())
+      extendedSaleState.minimumTier = extendedSaleState.minimumStatus;
+
+      let extendedRedeemableState: any = redeemableState;
+      // todo find a way to do away with this
+      extendedRedeemableState.erc20Config = {
+        name: extendedRedeemableState.name,
+        symbol: extendedRedeemableState.symbol,
+        distributor: ethers.constants.AddressZero,
+        initialSupply: ethers.utils.parseUnits(extendedRedeemableState.initialSupply.toString())
+      }
+
+
+      // todo might need to remove: price, saleTimeout, startBlock
+      // todo might need to remove initial supply (2nd one), minimum status, name, symbol, raiseRange, walletCap
+      delete extendedSaleState.price;
+      delete extendedSaleState.saleTimeout;
+      delete extendedSaleState.startBlock;
+      delete extendedRedeemableState.initialSupply;
+      // delete extendedRedeemableState.minimumStatus;
+      delete extendedRedeemableState.name;
+      delete extendedRedeemableState.symbol;
+      delete extendedRedeemableState.raiseRange;
+      delete extendedRedeemableState.walletCap;
+
+
+      console.log('Submitting the following state:', extendedSaleState, extendedRedeemableState);
+
+      const txDeploy = await saleFactory.createChildTyped(saleState, redeemableState);
+      const receipt = await txDeploy.wait();
+      const saleContractAddress = getNewChildFromReceipt(receipt, saleFactory);
+      console.log(saleContractAddress);
+
+      const sale = new ethers.Contract(
+        saleContractAddress,
+        saleContractABI.abi,
+        signer
+      ) as Contract;
+
+      if (!ethers.utils.isAddress(sale.address)) {
+        throw new Error(
+          `invalid sale address: ${sale.address} (${sale.address.length} chars)`
+        );
+      }
+
+      console.log(sale); // the sale contract
 
     } catch (err) {
       console.log(err);
